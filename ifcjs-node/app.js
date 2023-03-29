15df43d8f681f08da913c150a27bab2499526c65
Antonio Ulloa <joseanu@gmail.com>
2023-03-29 13:13:09 -0600

    ifcjs

---
+++ ifcjs-node/app.js
@@ -0,0 +1,49 @@
+const geometryTypes = require("./geometry-types").geometryTypes;
+const WebIFC = require('web-ifc/web-ifc-api-node');
+const fs = require("fs");
+
+const ifcapi = new WebIFC.IfcAPI();
+
+/* update IFC File path here; there are a few samples at ../../../../IFC */
+LoadFile("./vaz.ifc");
+
+async function LoadFile(filename) {
+    const modelID = await OpenIfc(filename);
+    const allItems = GetAllItems(modelID);
+
+    /* Update destination filename here */
+    fs.writeFileSync('./vaz.json', JSON.stringify(allItems, null, 2));
+    ifcapi.CloseModel(modelID);
+}
+
+async function OpenIfc(filename) {
+    const ifcData = fs.readFileSync(filename);
+    await ifcapi.Init();
+    return ifcapi.OpenModel(ifcData);
+}
+
+function GetAllItems(modelID, excludeGeometry = true) {
+    const allItems = {};
+    const lines = ifcapi.GetAllLines(modelID);
+    getAllItemsFromLines(modelID, lines, allItems, excludeGeometry);
+    return allItems;
+}
+
+function getAllItemsFromLines(modelID, lines, allItems, excludeGeometry) {
+    for (let i = 0; i < lines.size(); i++) {
+        try {
+            saveProperties(modelID, lines, allItems, excludeGeometry, i);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+function saveProperties(modelID, lines, allItems, excludeGeometry, index) {
+    const itemID = lines.get(index);
+    const props = ifcapi.GetLine(modelID, itemID);
+    props.type = props.__proto__.constructor.name;
+    if (!excludeGeometry || !geometryTypes.has(props.type)) {
+        allItems[itemID] = props;
+    }
+}
